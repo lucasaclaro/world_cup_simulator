0bbccd55696206fa3470fd4fba1ef6a2a6dc9eff
Lucas Claro <102222870+lucasaclaro@users.noreply.github.com>
2022-11-24 07:24:00 -0300

    Update teams.py

--- teams.py
+++ teams.py
@@ -1,48 +1,48 @@
 
 #Group A
 team_A = 'Qatar'
-team_B = 'Equador'
+team_B = 'Ecuador'
 team_C = 'Senegal'
-team_D = 'Holanda'
+team_D = 'Netherlands'
 
 #Group B
-team_E = 'Inglaterra'
-team_F = 'Irã'
-team_G = 'Estados Unidos'
-team_H = 'País de Gales'
+team_E = 'Ingland'
+team_F = 'IR Iran'
+team_G = 'USA'
+team_H = 'Wales'
 
 #Group C
 team_I = 'Argentina'
-team_J = 'Arábia Saudita'
-team_K = 'México'
-team_L = 'Polônia'
+team_J = 'Saudi Arabia'
+team_K = 'Mexico'
+team_L = 'Poland'
 
 #Group D
-team_M = 'França'
+team_M = 'France'
 team_N = 'Australia'
-team_O = 'Dinamarca'
-team_P = 'Tunísia'
+team_O = 'Dinmark'
+team_P = 'Tunisia'
 
 #Group E
-team_Q = 'Espanha'
+team_Q = 'Spain'
 team_R = 'Costa Rica'
-team_S = 'Alemanha'
-team_T = 'Japão'
+team_S = 'Germany'
+team_T = 'Japan'
 
 #Group F
-team_U = 'Bélgica'
-team_V = 'Canadá'
-team_W = 'Marrocos'
-team_X = 'Croácia'
+team_U = 'Belgium'
+team_V = 'Canada'
+team_W = 'Marocco'
+team_X = 'Croatia'
 
 #Group G
-team_Y = 'Brasil'
-team_Z = 'Sérvia'
-team_AA = 'Suíca'
-team_AB = 'Camarões'
+team_Y = 'Brazil'
+team_Z = 'Serbia'
+team_AA = 'Switzerland'
+team_AB = 'Camaroon'
 
 #Group H
 team_AC = 'Portugal'
-team_AD = 'Gana'
-team_AE = 'Uruguai'
-team_AF = 'Coréia do Sul'
+team_AD = 'Ghana'
+team_AE = 'Uruguay'
+team_AF = 'Korea Republic'
